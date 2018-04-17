1a9f4e41347749010a38e05a14bd6ef18d7ad139
Jackie <jacgut19@gmail.com>
2018-04-17 03:12:37 +0000

    mario more and less

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -4,76 +4,50 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-
 int main(int argc, string argv[])
 {
-    string plaintext; // var stores user message
-    string key = argv[1]; // variable stores key
-    int lastchar = 0; // last character examined
-    int counter = 0;
+    string plaintext;
 
-    if (argc != 2) // validate arguments
+    if (argc != 2 || strlen(argv[1]) == 0) // by default, argc will be 2 because ./caesar is the first argument
     {
         printf("Usage: ./vigenere k\n");
-        return 1; // value of 1 returns error
+        return 1; // stops progress, value of 1 returns error
+    }
+    int key = (argv[1]);
+    int klength = strlen(k);
+
+    // if (key < 0)
+    // {
+    //     printf("Enter positive number after filename \n");
+    //     return 1;
+    // }
+
+    plaintext = get_string("plaintext: "); // string variable user input
+    printf("ciphertext: ");
+    char ciphertext;
+    int counter = 0;
+
+    for (int i = 0, length = strlen(plaintext); i < length; i++)
+    {
+        ciphertext = plaintext[i];
+
+        if (isupper(plaintext[i])) // uppercase letters
+        {
+            ciphertext = (((plaintext[i] + key) - 65) % 26) + 65;// key counter
+        }
+
+        if (islower(plaintext[i])) // lowercase letters
+        {
+            ciphertext = (((plaintext[i] + key) - 97) % 26) + 97;
+        }
+
+        if (isalpha(plaintext[i])) // skip num when hashing
+        {
+            plaintext[i] = ciphertext;
+        }
+
+        printf("%c", ciphertext);
     }
 
-    for ( int i = 0; i < strlen(key); i++) // make sure keyword is from A-Z
-    {
-        if (!isalpha(argv[1][i])) // if it is not alphabetical
-        {
-            printf("Enter an alphabetical chars\n"); // tells the user to enter a letter
-            return 1;
-        }
-    }
-    do // loops through as long as message is NULL or empty
-    {
-        plaintext = get_string("plaintext: "); // get message from user
-
-    }while(plaintext == '\0'); // loop through message and manipulate string
-
-    for (int i = 0, j = strlen(plaintext); i < j; i++)
-    {
-        if (isalpha(plaintext[i]))
-        {
-            counter = lastchar % strlen(key); // set counter to modulo length of keyword
-
-            if (islower(plaintext[i])) // preserve lowercase
-            {
-                if (islower(key[counter]))
-                {
-                    plaintext[i] = ((plaintext[i] - 'a' + key[counter] - 97) % 26) + 97;
-                    printf("%c", plaintext[i]);
-                }
-                else
-                {
-                    plaintext[i] = ((plaintext[i] - 'a' + key[counter] - 65) % 26) + 97;
-                    printf("%c", plaintext[i]);
-                }
-            }
-
-            if (isupper(plaintext[i])) // preserve uppercase
-            {
-                if (islower(key[lastchar]))
-                {
-
-                    plaintext[i] = ((plaintext[i] - 'A' + key[counter] - 97) % 26) + 65;
-                    printf("%c", plaintext[i]);
-                }
-                else
-                {
-                    plaintext[i] = ((plaintext[i] - 'A' + key[counter] - 65) % 26) + 65;
-                    printf("%c", plaintext[i]);
-                }
-
-            }
-            lastchar++; // increment last characters by 1
-        }
-        else
-        {
-            printf("%c", plaintext[i]); // print encryted message
-        }
-    }
     printf("\n");
-    return 0;
 }
