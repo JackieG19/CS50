96f657639505e4edfbd74db12a996e9546b0f664
Jackie <jacgut19@gmail.com>
2018-04-18 00:53:36 +0000

    one error on check50

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -8,34 +8,38 @@ int main(int argc, string argv[])
 {
     string plaintext;
 // if argument is not equal to 2 OR the first argument is equal to 0
-    if (argc != 2 || strlen(argv[1]) == 0)
+    if (argc != 2)
     {
         printf("Usage: ./vigenere k\n");
         return 1; // stops progress, returns error
     }
     string k = argv[1]; // first argument
-    plaintext = get_string("plaintext: "); // string variable user input
-    printf("ciphertext: "); // output
-    int counter = 0; // value length of the string
 
-    for (int i = 0, length = strlen(plaintext); i < length; i++) //strlen() function calculates the length of input.
+    plaintext = get_string("plaintext: "); // string variable user input
+
+    printf("ciphertext: "); // output
+
+    int wlength = strlen(plaintext);
+
+    int klength = strlen(k); // grabing the string length of k
+
+    for (int i = 0, counter = 0; i < wlength; i++) //strlen() function calculates the length of input.
     {
-        int klength = strlen(k); // grabing the string length of k
+        int keyword = tolower(k[counter % klength]) - 'a'; // tracks occurrences of a character in the input
 
         if (isalpha(plaintext[i]))
         {
-            int key = k[counter] - 'A';
             if (isupper(plaintext[i]))
             {
-                printf("%c", ((plaintext[i] - 'A' + k[key]) % 26) + 'A');
+                printf("%c", (((plaintext[i] - 'A') + keyword) % 26) + 'A');
+                counter++;
             }
 
             else
             {
-                printf("%c", ((plaintext[i] - 'a' + k[key]) % 26) + 'a');
+                printf("%c", (((plaintext[i] - 'a') + keyword) % 26) + 'a');
+                counter++;
             }
-
-            key = (key) % klength;
         }
         else
         {
