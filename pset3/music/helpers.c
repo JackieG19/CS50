2e7fd9c757ca59cc4e4990d879e9ecc9ac2e3b80
Jackie <jacgut19@gmail.com>
2018-04-25 19:52:33 +0000

    works

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -9,26 +9,26 @@
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)   // Fraction (numerator--->1/4<---denominator)
 {
-    if(fraction[0] == '1')  // if numerator[top number of the fraction] equals to 1
+    if (fraction[0] == '1')  // if numerator[top number of the fraction] equals to 1
     {
-        if(fraction[2] == '8')  // if denominator[bottom number of the fraction] equals to a 8
+        if (fraction[2] == '8')  // if denominator[bottom number of the fraction] equals to a 8
         {
             return 1;
         }
-        else if(fraction[2] == '4')
+        else if (fraction[2] == '4')
         {
             return 2;
         }
-        else if(fraction[2] == '2')
+        else if (fraction[2] == '2')
         {
             return 4;
         }
-        else if(fraction[2] == '1')
+        else if (fraction[2] == '1')
         {
             return 8;
         }
     }
-    else if(fraction[0] == '3' && fraction[2] == '8')   // 3/8 fraction
+    else if (fraction[0] == '3' && fraction[2] == '8')   // 3/8 fraction
     {
         return 3;
     }
@@ -44,9 +44,9 @@ int frequency(string note) // Calculates frequency (in Hz) of a note
     int octave = note[strlen(note) - 1];
     octave -= 48; // adjusting to an ascii into an int value
 
-    double freq = 0.0;  // determine the frequency of note in the given octave
+    double freq = 0.0;  // the frequency of note in the given octave
 
-    switch(note[0]) // determine note letter
+    switch (note[0]) // determine note letter
     {
         case 'C' :
             freq = 440.0 / (pow(2.0, (9.0 / 12.0)));
@@ -73,27 +73,27 @@ int frequency(string note) // Calculates frequency (in Hz) of a note
             return 0;
     }
 
-    if(octave > 4)  // if octave num is greater than the 4th octave
+    if (octave > 4)  // if octave num is greater than the 4th octave
     {
-        for(int i = 0; i < octave - 4; i++)
+        for (int i = 0; i < octave - 4; i++)
         {
             freq *= 2.0;
         }
     }
-    else if(octave < 4) // if octave num is lesser than the 4th octave
+    else if (octave < 4) // if octave num is lesser than the 4th octave
     {
-        for(int i = 0; i < 4 - octave; i++)
+        for (int i = 0; i < 4 - octave; i++)
         {
             freq /= 2.0;
         }
     }
 
 
-    if(note[1] == 'b')  // if the note is a flat
+    if (note[1] == 'b')  // if the note is a flat
     {
         freq /= (pow(2.0, (1.0 / 12.0)));   // divide from 2 1/12
     }
-    else if(note[1] == '#') // if the note is a sharp
+    else if (note[1] == '#') // if the note is a sharp
     {
         freq *= (pow(2.0, (1.0 / 12.0)));   // multiply from 2 1/12
     }
@@ -106,7 +106,7 @@ int frequency(string note) // Calculates frequency (in Hz) of a note
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-     if (strlen(s) == 0)
+    if (strlen(s) == 0)
     {
         return true;
     }
