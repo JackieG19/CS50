1f98e847d69a34d268d193719f48d04ab66a0e35
Jackie <jacgut19@gmail.com>
2018-08-21 21:23:49 +0000

    hello, mario and cash done

---
+++ pset6/sentimental/cash.py
@@ -0,0 +1,31 @@
+from cs50 import get_float
+
+while True:
+        print("How much change is owed?")
+        amount = get_float()
+        if amount >= 0:
+            break
+
+coins = 0
+cents = round(amount * 100)
+
+coins += cents // 25
+cents %= 25
+
+coins += cents // 10
+cents %= 10
+
+coins += cents // 5
+cents %= 5
+
+coins += cents
+
+print("number of coins = ", coins)
+
+
+
+
+
+
+
+
---
+++ pset6/sentimental/hello.py
@@ -0,0 +1,7 @@
+import cs50
+
+print("hello, world")
+
+
+# python hello.py
+# prints out hello, world
