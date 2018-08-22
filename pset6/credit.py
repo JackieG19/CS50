6f54aa9bda91bd1fbd795b1499209c48a4d7e497
Jackie <jacgut19@gmail.com>
2018-08-22 01:01:27 +0000

    working on credit.py

---
+++ pset6/credit.py
@@ -0,0 +1,13 @@
+from cs50 import get_int
+
+while True:
+    print("Number:", end="")
+    creditcard = get_int()
+    if(creditcard >= 0):
+        break
+
+check(creditcard)
+
+check(cardnum)
+    length = get_length
+    if (checklength(length)) == True
